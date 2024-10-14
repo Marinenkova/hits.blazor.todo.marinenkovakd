80cd7a3dac8d242527430241d68894227b4b90bf
Marinenkova <marinenkova05@gmail.com>
2024-10-14 12:43:58 +0500

    4

--- TodoServerApp/Data/Services/MemoryDataService.cs
+++ TodoServerApp/Data/Services/MemoryDataService.cs
@@ -5,8 +5,8 @@ namespace TodoServerApp.Data.Services
     public class MemoryDataService : IDataService
     {
         private static IEnumerable<TaskItem> tasks { get; } =
-                [
-                    new() { Id = 1, Title = "Задача 1", Description = "Описание задачи 1", CreatedDate=DateTime.Now},
+        [
+            new() { Id = 1, Title = "Задача 1", Description = "Описание задачи 1", CreatedDate=DateTime.Now},
             new() { Id = 2, Title = "Задача 2", Description = "Описание задачи 2", CreatedDate=DateTime.Now},
             new() { Id = 3, Title = "Задача 3", Description = "Описание задачи 3", CreatedDate=DateTime.Now},
         ];
