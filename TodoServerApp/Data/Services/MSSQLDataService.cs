94c68b01d0a304d0f929c459a8255a48a9976850
Marinenkova <marinenkova05@gmail.com>
2024-10-25 19:09:56 +0500

    6

--- TodoServerApp/Data/Services/MSSQLDataService.cs
+++ TodoServerApp/Data/Services/MSSQLDataService.cs
@@ -5,9 +5,25 @@ namespace TodoServerApp.Data.Services
 {
 	public class MSSQLDataService(ApplicationDbContext context) : IDataService
 	{
-		public async Task<IEnumerable<TaskItem>> GetTaskItemsAsync()
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TaskItem> GetTaskAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<TaskItem>> GetTaskItemsAsync()
 		{
 			return await context.TaskItems.ToArrayAsync();
 		}
-	}
+
+        public async Task SaveAsync(TaskItem item)
+        {
+            await context.TaskItems.AddAsync(item);
+            await context.SaveChangesAsync();
+        }
+    }
 }
